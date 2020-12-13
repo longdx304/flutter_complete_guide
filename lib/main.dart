f6f1c8c2632fa349cae6a3aa35470d844d742d0d
Long Do <longdo@dobtech.vn>
2020-12-13 16:11:01 +0700

    add score to answer and display result

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,37 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Red', 'White', 'Blue', 'Yellow'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Lion', 'Dog', 'Cat'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
   int _questionIndex = 0;
+  int _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() => _questionIndex++);
   }
 
@@ -42,7 +63,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int totalScore;
+
+  Result(this.totalScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (totalScore <= 8)
+      resultText = 'You are awesome and innocent!';
+    else if (totalScore <= 12)
+      resultText = 'Pretty likeable';
+    else if (totalScore <= 16)
+      resultText = 'You are ... strange?!';
+    else
+      resultText = 'You are so bad!';
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }

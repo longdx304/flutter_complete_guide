7a329574aff8df0e97353128fc380e61ab9641c8
Long Do <longdo@dobtech.vn>
2020-12-13 16:25:27 +0700

    add ability to reset quiz

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int totalScore;
+  final Function resetHandler;
 
-  Result(this.totalScore);
+  Result(this.totalScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -21,13 +22,22 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            textColor: Colors.blue,
+            child: Text('Restart Quiz!'),
+            onPressed: resetHandler,
+          ),
+        ],
       ),
     );
   }
